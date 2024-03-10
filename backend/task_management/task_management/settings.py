6cbdfbb64e5cb7aef613b550de4740cb76046b27
Jeremy Lehmann <jeremy.lehmann06@icloud.com>
2024-03-10 10:27:54 -0700

    Set up Django app

--- backend/task_management/task_management/settings.py
+++ backend/task_management/task_management/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
