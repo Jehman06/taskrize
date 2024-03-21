24f8ccce12208364538026eb408eade4fbc44d59
Jeremy Lehmann <jeremy.lehmann06@icloud.com>
2024-03-21 09:06:57 -0700

    Added a useEffect in Home.tsx that resets the app states

--- frontend/task-management/src/Home/Home.tsx
+++ frontend/task-management/src/Home/Home.tsx
@@ -3,12 +3,19 @@ import { UseSelector, useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import PrivateNavbar from '../Navbar/PrivateNavbar';
 import { spiral } from 'ldrs';
+import { resetAppStates } from '../redux/reducers/appSlice';
 
 const Home: React.FC = () => {
   // Access the authenticated user's data from the Redux store
   const user = useSelector((state: RootState) => state.auth.user);
+  const dispatch = useDispatch();
 
   spiral.register();
+
+  // Reset app states, especially loading
+  useEffect(() => {
+    dispatch(resetAppStates());
+  });
 
   if (!user) {
     return (
