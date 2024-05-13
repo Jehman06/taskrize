08b1ce61fe2db7bdcfac37dcff80b095bba067d9
Jeremy Lehmann <jeremy.lehmann06@icloud.com>
2024-05-13 16:56:04 -0700

    Set up ASGI for deployment

--- backend/task_management/task_management/asgi.py
+++ backend/task_management/task_management/asgi.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.task_management.task_management.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_management.settings')
 django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
